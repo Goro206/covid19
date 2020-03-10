de4ea6fe6cc23ea0f87f741852706ef882ff1f31
Goro206 <Yoshiyuki.Inagaki.peugeot@gmail.com>
2020-03-11 02:19:51 +0900

    Change "theme_color" to Dark Red

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -174,7 +174,7 @@ const config: Configuration = {
   },
   manifest: {
     name: '愛知県 新型コロナウイルス感染症対策サイト',
-    theme_color: '#00a040',
+    theme_color: '#A63C47',
     background_color: '#ffffff',
     display: 'standalone',
     Scope: '/',
